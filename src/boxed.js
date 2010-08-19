b2ec114b8ad48de18f6a8572cdb49191690755a6
Jacob Persson <jacob.persson@gmail.com>
2010-08-20 00:29:28 +0200

    replaced raphaeljs with a combination of canvas elements and css transforms

--- src/boxed.js
+++ src/boxed.js
@@ -25,196 +25,31 @@ function boundingBoxTest(box1, box2) {
 	return true;
 }
 
-Number.prototype.mod = function(n) {
-	return ((this % n) + n) % n;
-}
-
-Function.prototype.curry = function() {
-	var fn = this, args = Array.prototype.slice.call(arguments);
-	return function() {
-		return fn.apply(this, args.concat(Array.prototype.slice.call(arguments)));
-	};
-}
-
-Function.prototype.bindScope = function(scope) {
-	var fn = this;
-	return function() {
-		return fn.apply(scope, arguments);
-	};
-}
-
-Raphael.fn.block = function(x, y, matrix, blockSize) {
-	
-	var lookup = [
-		{ x:  1, y:  0 },
-		{ x:  0, y:  1 },
-		{ x: -1, y:  0 },
-		{ x:  0, y: -1 },
-	];
-	
-	var rotateLeft = function(vector) {
-		for (var i = 0; i < 4; i++) {
-			if (lookup[i].x == vector.x && lookup[i].y == vector.y) {
-				return lookup[(4+i-1) % 4];
-			}				
-		}
-	}
-	
-	var rotateRight = function(vector) {
-		for (var i = 0; i < 4; i++) {
-			if (lookup[i].x == vector.x && lookup[i].y == vector.y) {
-				return lookup[(4+i+1) % 4];
-			}				
-		}
-	}
-	
-	var translate = function (point, vector) {
-		return { x: point.x + vector.x, y: point.y + vector.y };
-	}
-	
-	var isEqual = function(vector1, vector2) {
-		return vector1.x == vector2.x && vector1.y == vector2.y;
-	}
-	
-	var get = function(x, y) {
-		var row = matrix[y];
-		
-		if (row) {
-			return row[x];
-		} else {
-			return null;
-		}
-	}
-	
-	var findBlock = function(matrix) {
-		for (var i = 0; i < matrix.length; i++) {
-			for (var j = 0; j < matrix[i].length; j++) {
-				if (matrix[i][j]) {
-					return { x: j, y: i };
-				}
-			}
-		}
-	}
-	
-	// find first active block	
-	var start = findBlock(matrix);
-		
-	var direction = {x: 1, y: 0 }
-	var position = start;	
-	var path = this.path("");
-	var turns = 0;
-	
-	path.relatively();
-	path.moveTo(direction.x * blockSize, direction.y * blockSize);
-
-	do {
-		var ff = translate(position, direction);
-		var rr = translate(position, rotateLeft(direction));
-		var fr = translate(ff, rotateLeft(direction));
-					
-		var ffValue = get(ff.x, ff.y);
-		var rrValue = get(rr.x, rr.y);
-		var frValue = get(fr.x, fr.y);
-	
-		if (rrValue && turns == 0) {
-			// turn right
-			direction = rotateLeft(direction);
-			turns++;
-		} else 
-		if (ffValue) {
-			// go forward
-			position = ff;
-			turns = 0;
-			if (!frValue) {
-				path.lineTo(direction.x * blockSize, direction.y * blockSize);
-			}
-		} else {
-			// turn left
-			direction = rotateRight(direction);
-			path.lineTo(direction.x * blockSize, direction.y * blockSize);
-			turns++;
-		}
-		
-	} while (!(isEqual(position, start) && isEqual(direction, { x: 1, y: 0 })));
-	
-	return path.andClose();
-}
-
-Raphael.el.setPosition = function(x, y) {
-	var c = this.attr("translation");
-	this.translate(x - c.x, y - c.y);
-}
-
-function Boxed(containerElement) {
-	this.pieces = [];
-	this.paper = Raphael(containerElement, containerElement.offsetWidth, containerElement.offsetHeight);
-	this.blockSize = 40;
-	
-	this.addPiece(Shapes.L, 0, 0);
-	this.addPiece(Shapes.T, 200, 100);
-}
-
-Boxed.prototype = {
-	
-	resize: function (width, height) {
-		this.paper.setSize(width, height);
-	},
-		
-	addPiece: function (shape, x, y) {
-
-		var block = this.paper.block(0, 0, shape, this.blockSize).attr({
-				fill: "hsb(.6, 1, 1)",
-				stroke: "hsb(0, 0, 0)"
-			});
-		block.setPosition(x, y);
-
-		var piece = new Piece(this, block);
-		piece.mshape = shape;
-		this.pieces.push(piece);
-	}
-	
-};
-
-function Piece(boxed, block) {
-	this.boxed = boxed;
-	this.block = block;
-	
-	block.drag(this.moved.bindScope(this), this.grabbed.bindScope(this), this.released.bindScope(this));
-}
-
-Piece.prototype.resolveCollision = function(aPiece) {
-	var bbox1 = this.block.getBBox();
-	var bbox2 = aPiece.block.getBBox();
-	var boxed = this.boxed;
+function getBlockCollisionResolution(block1, block2, blockSize) {
+	var bbox1 = block1.getBBox();
+	var bbox2 = block2.getBBox();
 	
 	var xdelta = bbox1.x - bbox2.x;
 	var ydelta = bbox1.y - bbox2.y;
-	
-	//console.log(["xdelta: ", xdelta, " ydelta: ", ydelta].join(''));
-	
-	var xoffset =  boxed.blockSize - xdelta.mod(boxed.blockSize);
-	var yoffset =  boxed.blockSize - ydelta.mod(boxed.blockSize);
+		
+	var xoffset =  boxed.blockSize - xdelta.mod(blockSize);
+	var yoffset =  boxed.blockSize - ydelta.mod(blockSize);
 	var rxoffset = boxed.blockSize - xoffset;
 	var ryoffset = boxed.blockSize - yoffset;
-	
-	//var xoffset2 = bbox2.x.mod(boxed.blockSize);
-	//var yoffset2 = bbox2.y.mod(boxed.blockSize);
-	
-	var len = this.mshape.length;
 		
-	var m = xdelta / boxed.blockSize;
-	var n = ydelta / boxed.blockSize;
-	
-	//console.log(["m: ", m, " n: ", n].join(''));
-	
+	var len = block1.matrix.length;
+		
+	var m = xdelta / blockSize;
+	var n = ydelta / blockSize;
+		
 	function check(x, y) {
 		if (x >= 0 && x < len && y >= 0 && y < len) {
-			return aPiece.mshape[y][x];
+			return block2.matrix[y][x];
 		}
 		return 0;
 	}
 	
-	var resolve = { x: 0, y: 0 };
+	var resolution = { x: 0, y: 0 };
 	var max = 0;
 	
 	//console.log("###");
@@ -222,7 +57,7 @@ Piece.prototype.resolveCollision = function(aPiece) {
 	for (var i = 0; i < len; i++) {
 		for (var j = 0; j < len; j++) {
 			
-			if (!this.mshape[j][i]) {
+			if (!block1.matrix[j][i]) {
 				continue;
 			}
 			
@@ -241,105 +76,327 @@ Piece.prototype.resolveCollision = function(aPiece) {
 				console.log("c: " + c);
 				console.log("d: " + d);
 				console.log("---");
-			}
-			*/
+			} */
 						
 			if (a + b + c + d == 1) {
 				if (a) {
 					if (xoffset < yoffset) {
-						resolve.x = xoffset;
+						resolution.x = xoffset;
 					} else {
-						resolve.y = yoffset;
+						resolution.y = yoffset;
 					}
 				} else
 				if (b) {
 					if (rxoffset < yoffset) {
-						resolve.x = -rxoffset;
+						resolution.x = -rxoffset;
 					} else {
-						resolve.y = yoffset;
+						resolution.y = yoffset;
 					}
 				} else
 				if (c) {
 					if (xoffset < ryoffset) {
-						resolve.x = xoffset
+						resolution.x = xoffset
 					} else {
-						resolve.y = -ryoffset;
+						resolution.y = -ryoffset;
 					}
 				} else {
 					if (rxoffset < ryoffset) {
-						resolve.x = -rxoffset;
+						resolution.x = -rxoffset;
 					} else {
-						resolve.y = -ryoffset;
+						resolution.y = -ryoffset;
 					}
 				}
 			}
 			else {
 				if (a && b) {
-					resolve.y = yoffset;
+					resolution.y = yoffset;
 				} else
 				if (d && c) {
-					resolve.y = -ryoffset;
+					resolution.y = -ryoffset;
 				}
 
 				if (b && d) {
-					resolve.x = -rxoffset;
+					resolution.x = -rxoffset;
 				} else
 				if (c && a) {
-					resolve.x = xoffset;
+					resolution.x = xoffset;
 				}	
 			}
 		}
 	}
-	
-	//console.log("xoffset: " + xoffset);
-	//console.log("yoffset: " + yoffset);
-	//console.log(["resolve(", resolve.x, ",", resolve.y, ")"].join(''));
-	
-	var position = this.block.attr("translation");
-	this.block.setPosition(position.x + resolve.x, position.y + resolve.y);
+		
+	return resolution;
 }
 
-Piece.prototype.grabbed = function() {
-	this.initialPosition = this.block.attr("translation");
-	this.lastPosition = this.block.attr("translation");
-	this.ddx = 0;
-	this.ddy = 0;
+Number.prototype.mod = function(n) {
+	return ((this % n) + n) % n;
 }
 
-Piece.prototype.moved = function(dx, dy) {
-	this.lastPosition = this.block.attr("translation");
+Function.prototype.curry = function() {
+	var fn = this, args = Array.prototype.slice.call(arguments);
+	return function() {
+		return fn.apply(this, args.concat(Array.prototype.slice.call(arguments)));
+	};
+}
+
+Function.prototype.bindScope = function(scope) {
+	var fn = this;
+	return function() {
+		return fn.apply(scope, arguments);
+	};
+}
+
+function Boxed(containerElement) {
+	this.container = containerElement;
+	this.pieces = [];
+	this.blockSize = 40;
 	
-	var x = this.lastPosition.x + (dx - this.ddx);
-	var y = this.lastPosition.y + (dy - this.ddy);
+	this.addPiece(Shapes.L, 0, 0);
+	this.addPiece(Shapes.T, 0, 160);
+}
+
+Boxed.prototype = {
 	
-	this.ddx = dx;
-	this.ddy = dy;
+	resize: function (width, height) {
+		//this.paper.setSize(width, height);
+	},
+		
+	addPiece: function (shape, x, y) {
+		var block = new Block(this.container, shape, this.blockSize);
+		block.setPosition(x, y);
+		this.pieces.push(block);
+		
+		block.drag(
+			function () {
+				block.initialPosition = block.getPosition();
+				block.lastPosition = block.getPosition();
+				block.ddx = 0;
+				block.ddy = 0;
+			}.bindScope(this),
+			function (dx, dy) {
+				block.lastPosition = block.getPosition();
+
+				var x = block.lastPosition.x + (dx - block.ddx);
+				var y = block.lastPosition.y + (dy - block.ddy);
+				
+				block.ddx = dx;
+				block.ddy = dy;
+				block.setPosition(x, y);
+				
+				for (i in this.pieces) {
+					var piece = this.pieces[i];
+					
+					if (piece != block) {
+						if (boundingBoxTest(block.getBBox(), piece.getBBox())) {							
+							resolution = getBlockCollisionResolution(block, piece, this.blockSize);
+							block.setPosition(x + resolution.x, y + resolution.y);
+						}
+					}
+				}
+			}.bindScope(this),
+			function() {
+
+		});
+	}
 	
-	this.block.setPosition(x, y);
-	var pieces = this.boxed.pieces;
+};
+
+
+function Block(container, matrix, blockSize) {
+	this.matrix = matrix;
+	this._size = this._calculateSize(matrix, blockSize);
+	this.canvas = container.appendChild(this._createCanvas(this._size.width, this._size.height));
+	this.ctx = this.canvas.getContext("2d");
+	this._render(this.ctx, matrix, blockSize);
+}
+
+Block.layerCount = 0;
+
+Block.prototype = {
 	
-	for (i in pieces) {
-		var piece = pieces[i];
-		if (piece != this) {
-			if (boundingBoxTest(this.block.getBBox(), piece.block.getBBox())) {
-				this.resolveCollision(piece);
+	_createCanvas: function (width, height) {		
+		Block.layerCount += 1;
+		var canvas = document.createElement('canvas');
+		canvas.setAttribute('class', 'block');
+		canvas.setAttribute('style', 'z-index: ' + Block.layerCount);
+		canvas.setAttribute('width', width);
+		canvas.setAttribute('height', height);
+		return canvas;
+	},
+	
+	_render: function (ctx, matrix, blockSize) {
+		var lookup = [
+			{ x:  1, y:  0 },
+			{ x:  0, y:  1 },
+			{ x: -1, y:  0 },
+			{ x:  0, y: -1 },
+		];
+
+		var rotateLeft = function(vector) {
+			for (var i = 0; i < 4; i++) {
+				if (lookup[i].x == vector.x && lookup[i].y == vector.y) {
+					return lookup[(4+i-1) % 4];
+				}				
 			}
 		}
-	}
-}
 
-Piece.prototype.released = function() {
-	var blockSize = this.boxed.blockSize
-	var position = this.block.attr("translation");
+		var rotateRight = function(vector) {
+			for (var i = 0; i < 4; i++) {
+				if (lookup[i].x == vector.x && lookup[i].y == vector.y) {
+					return lookup[(4+i+1) % 4];
+				}				
+			}
+		}
+
+		var translate = function (point, vector) {
+			return { x: point.x + vector.x, y: point.y + vector.y };
+		}
+
+		var isEqual = function(vector1, vector2) {
+			return vector1.x == vector2.x && vector1.y == vector2.y;
+		}
+
+		var get = function(x, y) {
+			var row = matrix[y];
+
+			if (row) {
+				return row[x];
+			} else {
+				return null;
+			}
+		}
+
+		var findBlock = function(matrix) {
+			for (var i = 0; i < matrix.length; i++) {
+				for (var j = 0; j < matrix[i].length; j++) {
+					if (matrix[i][j]) {
+						return { x: j, y: i };
+					}
+				}
+			}
+		}
+
+		// find first active block	
+		var start = findBlock(matrix);
+
+		var turns = 0;
+		var direction = {x: 1, y: 0 };
+		var position = start;
+		var gposition = { x: position.x * blockSize, y: position.y * blockSize };
+		gposition = translate(gposition, { x: direction.x * blockSize, y: direction.y * blockSize });
+
+		ctx.fillStyle = "rgb(200,0,0)";
+		ctx.beginPath();
+		ctx.moveTo(gposition.x, gposition.y);
+
+		do {
+			var ff = translate(position, direction);
+			var rr = translate(position, rotateLeft(direction));
+			var fr = translate(ff, rotateLeft(direction));
+
+			var ffValue = get(ff.x, ff.y);
+			var rrValue = get(rr.x, rr.y);
+			var frValue = get(fr.x, fr.y);
+
+			if (rrValue && turns == 0) {
+				// turn right
+				direction = rotateLeft(direction);
+				turns++;
+			} else 
+			if (ffValue) {
+				// go forward
+				position = ff;
+				turns = 0;
+				if (!frValue) {
+					//ctx.lineTo(direction.x * blockSize, direction.y * blockSize);
+					gposition = translate(gposition, { x: direction.x * blockSize, y: direction.y * blockSize });
+					ctx.lineTo(gposition.x, gposition.y);
+				}
+			} else {
+				// turn left
+				direction = rotateRight(direction);
+				//ctx.lineTo(direction.x * blockSize, direction.y * blockSize);
+				gposition = translate(gposition, { x: direction.x * blockSize, y: direction.y * blockSize });
+				ctx.lineTo(gposition.x, gposition.y);
+				turns++;
+			}
+
+		} while (!(isEqual(position, start) && isEqual(direction, { x: 1, y: 0 })));
+
+		ctx.closePath();
+		ctx.fill();
+	},
+	
+	_calculateSize: function(matrix, blockSize) {
+		return { width: matrix[0].length * blockSize, height: matrix.length * blockSize };
+	},
+	
+	setPosition: function (x, y) {
+		this._x = x;
+		this._y = y;
+		this.canvas.style.webkitTransform = ['translate3d(', x, 'px,', y, 'px,', '0px)'].join('');
+	},
+	
+	getPosition: function() {
+		return {x: this._x, y: this._y };
+	},
+	
+	getBBox: function() {
+		return { x: this._x, y: this._y, width: this._size.width, height: this._size.height };
+	},
 		
-	var xoffset = position.x.mod(blockSize);
-	var yoffset = position.y.mod(blockSize);
+	drag: function(grabbed, moved, released) {		
+		var canvas = this.canvas;
+		var self = this;
+		var touchSupport = "createTouch" in document;
 		
-	var correction = { x: 0, y: 0 };
-	correction.x += xoffset < blockSize / 2 ? -xoffset : (blockSize - xoffset);
-	correction.y += yoffset < blockSize / 2 ? -yoffset : (blockSize - yoffset);
+		var down = "mousedown";
+		var move = "mousemove";
+		var up = "mouseup";
 		
-	this.block.animate({ translation: [correction.x , correction.y].join(' ') }, 200, "backIn");
+		if (touchSupport) {
+			down = "touchstart";
+			move = "touchmove";
+			up = "touchend";
+		}
+		
+		function _moved(event) {
+			event.preventDefault();
+			
+			if (touchSupport) {
+				var dx = event.targetTouches[0].pageX - self._startX;
+			    var dy = event.targetTouches[0].pageY - self._startY;
+			} else {
+				var dx = event.pageX - self._startX;
+				var dy = event.pageY - self._startY;
+			}
+			
+			moved(dx, dy);
+		}
+		
+		function _released(event) {
+			document.removeEventListener(move, _moved, false);
+			document.removeEventListener(up, _released, false);
+			released();
+		}
+		
+		canvas.addEventListener(down, function (event) {
+			event.preventDefault();
+			grabbed();
+			
+			if (touchSupport) {
+				self._startX = event.targetTouches[0].pageX;
+			    self._startY = event.targetTouches[0].pageY;
+			} else {
+				self._startX = event.pageX;
+				self._startY = event.pageY;
+			}
+			
+			document.addEventListener(move, _moved, false);
+			document.addEventListener(up, _released, false);			
+		});
+	}
+		
 }
 
 var Shapes = {
@@ -352,6 +409,6 @@ var Shapes = {
 	T: [[0, 1, 0, 0],
 		[0, 1, 1, 0],
 		[0, 1, 1, 0],
-		[1, 1, 1, 1]]
+		[0, 1, 1, 1]]
 		
 }
