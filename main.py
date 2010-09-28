940cd146ccc5c4bc39183e17b0040364027cd485
Jacob Persson <jacob.persson@gmail.com>
2010-09-29 01:08:02 +0200

    Add feedback form/dialog

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import mail
 
 class MainPage(webapp.RequestHandler):
   def get(self):
     path = os.path.join(os.path.dirname(__file__), 'index.html')
     self.response.out.write(template.render(path, {}))
 
-application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+class Feedback(webapp.RequestHandler):
+	def post(self):
+		mail.send_mail(sender="All These Boxes <feedback@alltheseboxes.com>",
+					   to="Jacob Persson <jacob.persson@robotcrowd.com>",
+					   subject="Feedback Response",
+					   body=self.request.get("message"))
+
+application = webapp.WSGIApplication([('/', MainPage),
+									  ('/feedback', Feedback),
+									 ],
+									 debug=True)
 
 def main():
   run_wsgi_app(application)
